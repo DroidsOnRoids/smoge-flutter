873e91561279ce69df59bd7c27a635285c7338ac
Alex S <alexander.stolar@yahoo.com>
2020-08-26 23:28:59 +0200

    Triggering getFirstStation method only once

--- lib/domain/provider/provider_model_async_result.dart
+++ lib/domain/provider/provider_model_async_result.dart
@@ -5,16 +5,16 @@ import 'package:async/async.dart';
 class ProviderModelAsyncResult<T> {
   Future<void> set(
       Future<Result<T>> futureResult, VoidCallback isLoadingChanged) async {
-    isLoading = true;
+    _isLoading = true;
 
     result = await futureResult;
 
-    isLoading = false;
+    _isLoading = false;
     isLoadingChanged();
   }
 
-  bool get hasData => !isLoading || result != null;
+  bool get hasData => !_isLoading || result != null;
 
   Result<T> result;
-  bool isLoading = false;
+  bool _isLoading = false;
 }
